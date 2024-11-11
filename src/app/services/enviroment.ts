2a10aa7110b8feaedd0a9d47ad586145549b28fc
Gustavo Lara <gustavo.silvalara@outlook.com>
2024-11-10 21:42:25 -0400

    ok

--- src/app/services/enviroment.ts
+++ src/app/services/enviroment.ts
@@ -1,6 +1,9 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://187.123.24.94:8081',
-    wsUrl: 'ws://187.123.24.94:8081/ws',
-    jsonServerUrl: 'http://187.123.24.94:3000'
+    apiUrl: 'http://localhost:8081',
+    wsUrl: 'ws://localhost:8081/ws',
+    jsonServerUrl: 'http://localhost:3000'
+    // apiUrl: 'http://187.123.24.94:8081',
+    // wsUrl: 'ws://187.123.24.94:8081/ws',
+    // jsonServerUrl: 'http://187.123.24.94:3000'
   };
